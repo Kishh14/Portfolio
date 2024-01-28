109af3559476e6c5c7f713b8b216bfa92bfb61f3
Kishh (Kishan) Salvi <88472719+Kishh14@users.noreply.github.com>
2024-01-28 22:59:17 +0530

    Uploading files

---
+++ assets/js/projects.js
@@ -0,0 +1,148 @@
+const projectsData = [
+  {
+    id: 1,
+    title: `Amazon Clone: E-commerce Website`,
+    link: "https://amazon-clone-kishh.netlify.app/",
+    image: "./assets/images/projects/amazone-clone.png",
+    description: `A fully responsive, multi-page replica of the
+    Amazon e-commerce platform. Featuring clean design and interactive product listings,
+    it's a practice project built with HTML5, CSS, and Bootstrap5.`,
+    techStack: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+    category: ["Clones", "Ecommerce Websites"],
+  },
+  {
+    id: 2,
+    title: "WhatsApp Clone: Messaging App",
+    link: "https://whatsapp-clone-kishh.netlify.app/",
+    image: "./assets/images/projects/whatsapp-clone.png",
+    description: `A faithful clone of the popular messaging app
+    WhatsApp, this project mirrors its functionality using HTML5, CSS3, and Vanilla
+    JavaScript. With a clean interface and user-friendly features.`,
+    techStack: ["HTML", "CSS", "JavaScript"],
+    category: ["Clones"],
+  },
+  {
+    id: 3,
+    title: "Kishha: E-commerce Bliss",
+    link: "https://kishha-ecommerce-kishh.netlify.app/",
+    image: "./assets/images/projects/kishha-eccomerce.png",
+    description: `A dynamic 5-page e-commerce site crafted with
+    HTML5, CSS3, and Vanilla JS. The homepage introduces a captivating shopping
+    experience, while the shop, contact, about, and single product pages contribute to a
+    seamless and responsive design.`,
+    techStack: ["HTML", "CSS", "JavaScript"],
+    category: ["Ecommerce Websites"],
+  },
+  {
+    id: 4,
+    title: "Kickup Golf: Family-Friendly Landing",
+    link: "https://kickup-golf-kishh.netlify.app/",
+    image: "./assets/images/projects/sidcup-golf.png",
+    description: `A captivating landing page for "Kickup Family Golf"
+    designed with HTML5, CSS3, Vanilla JS, and GSAP. This responsive site introduces a
+    family golf club with modern animations, seamlessly blending information about golf in a
+    single-page experience.`,
+    techStack: ["HTML", "CSS", "JavaScript", "GSAP"],
+    category: ["Animated Websites", "Landing Pages"],
+  },
+  {
+    id: 5,
+    title: "Gurukul Haven: Educational Retreat",
+    link: "https://gurukul-ashram-kishh.netlify.app/",
+    image: "./assets/images/projects/gurukul-website.png",
+    description: `An educational sanctuary seamlessly designed with
+    HTML5, Bootstrap5, and CSS. Dive into the essence of traditional learning with a
+    responsive homepage
+    and an enlightening about page. The timeless wisdom and educational ethos that
+    Gurukul Haven offers, providing a serene glimpse into the world of holistic education.`,
+    techStack: ["HTML", "CSS", "Bootstrap", "AOS"],
+    category: ["Landing Pages"],
+  },
+  {
+    id: 6,
+    title: "Japanese Sushi: Culinary Delight",
+    link: "https://japanese-sushi-kishh.netlify.app/",
+    image: "./assets/images/projects/japanese-sushi.png",
+    description: `An enticing landing page for "Japanese Sushi"
+    restaurant, skillfully crafted using HTML5 and CSS. This responsive site presents the
+    restaurant's menu and additional information, providing a delightful glimpse into the
+    world of Japanese cuisine.`,
+    techStack: ["HTML", "CSS"],
+    category: ["Landing Pages", "Ecommerce Websites"],
+  },
+];
+
+// Add active class to the navbar
+const navBtns = document.querySelectorAll(".nav-btn");
+
+navBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    navBtns.forEach((btn) => btn.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
+
+const populateAllProjects = () => {
+  const projectContainer = document.querySelector(".projects-container");
+  let projectsHtml = ``;
+
+  projectsData.map((project) => {
+    projectsHtml += `
+      <div class="col-12 col-md-6 col-lg-4 project text-white">
+      <a class="d-block text-decoration-none" href="${project.link}"
+          target="_blank" rel="noopener">
+          <img src="${project.image}" alt="${project.title}" class="m-auto d-block"
+              style="width: 90%; height: auto; border-radius: 13px;">
+      </a>
+      <h2 class="px-3 px-md-4 ps-lg-5 fw-bolder mb-2 mt-4">
+          <a class="d-block text-decoration-none text-white"
+              href="${project.link}" target="_blank" rel="noopener"
+              style="font-size: 22px; white-space: nowrap;">
+              ${project.title}
+          </a>
+      </h2>
+      <p class="px-4 px-md-4 ps-lg-5 m-0 fs-5">
+      ${project.description}
+      </p>
+      </div>`;
+  });
+  projectContainer.innerHTML = projectsHtml;
+};
+
+const sortProjects = (category, element) => {
+  const projectsContainer = document.querySelector(".projects-container");
+  let sortedProjects = [];
+  let projectHtml = ``;
+
+  projectsData.forEach((project, index) => {
+    if (project.category.includes(category)) {
+      sortedProjects.push(project);
+    } else {
+    }
+  });
+
+  sortedProjects.map((project) => {
+    projectHtml += `
+    <div class="col-12 col-md-6 col-lg-4 project text-white">
+    <a class="d-block text-decoration-none" href="${project.link}"
+        target="_blank" rel="noopener">
+        <img src="${project.image}" alt="${project.title}" class="m-auto d-block"
+            style="width: 90%; height: auto; border-radius: 13px;">
+    </a>
+    <h2 class="px-3 px-md-4 ps-lg-5 fw-bolder mb-2 mt-4">
+        <a class="d-block text-decoration-none text-white"
+            href="${project.link}" target="_blank" rel="noopener"
+            style="font-size: 22px; white-space: nowrap;">
+            ${project.title}
+        </a>
+    </h2>
+    <p class="px-4 px-md-4 ps-lg-5 m-0 fs-5">
+    ${project.description}
+    </p>
+    </div>`;
+  });
+
+  projectsContainer.innerHTML = projectHtml;
+};
+
+populateAllProjects();
---
+++ assets/js/script.js
@@ -0,0 +1,116 @@
+let landingPagePackagesContainer = document.getElementById(
+  "landing-page-packages"
+);
+let fullWebsitePackagesContainer = document.getElementById(
+  "full-website-packages"
+);
+let ecommerceWebsitePackagesContainer = document.getElementById(
+  "ecommerce-website-packages"
+);
+
+const showPackages = (button) => {
+  let card = button.closest(".card");
+  let choosenService = card.querySelector("h4");
+  console.log(button);
+  let serviceNameInput = document.getElementById("service-name-input");
+
+  if (choosenService.textContent === "Landing Page Creation") {
+    landingPagePackagesContainer.style.display = "block";
+    button.href = "#landing-page-packages";
+
+    fullWebsitePackagesContainer.style.display = "none";
+    ecommerceWebsitePackagesContainer.style.display = "none";
+
+    // Change the modal form input name based on the choosen service
+    serviceNameInput.setAttribute("name", "Landing Page");
+  } else if (choosenService.textContent === "Full Website Frontend") {
+    fullWebsitePackagesContainer.style.display = "block";
+    button.href = "#full-website-packages";
+
+    landingPagePackagesContainer.style.display = "none";
+    ecommerceWebsitePackagesContainer.style.display = "none";
+
+    // Change the modal form input name based on the choosen service
+    serviceNameInput.setAttribute("name", "Full Frontend Website");
+  } else if (choosenService.textContent === "Ecommerce Frontend") {
+    ecommerceWebsitePackagesContainer.style.display = "block";
+    button.href = "#ecommerce-website-packages";
+
+    landingPagePackagesContainer.style.display = "none";
+    fullWebsitePackagesContainer.style.display = "none";
+
+    // Change the modal form input name based on the choosen service
+    serviceNameInput.setAttribute("name", "Ecommerce Website Frontend");
+  }
+};
+
+const showModal = (button) => {
+  let packageDiv = button.closest(".package");
+  let choosenPackage = packageDiv.querySelector("h3");
+  console.log(choosenPackage.textContent);
+
+  let landingBasicPackageRadio = document.getElementById(
+    "radio-landing-basic-package"
+  );
+  let landingStandardPackageRadio = document.getElementById(
+    "radio-landing-standard-package"
+  );
+  let landingPremiumPackageRadio = document.getElementById(
+    "radio-landing-premium-package"
+  );
+  if (choosenPackage.textContent === "Basic Package") {
+    landingStandardPackageRadio.removeAttribute("checked");
+    landingPremiumPackageRadio.removeAttribute("checked");
+    landingStandardPackageRadio.setAttribute("disabled", "true");
+    landingPremiumPackageRadio.setAttribute("disabled", "true");
+
+    landingBasicPackageRadio.removeAttribute("disabled");
+    landingBasicPackageRadio.setAttribute("checked", "true");
+  }
+  if (choosenPackage.textContent === "Standard Package") {
+    landingBasicPackageRadio.removeAttribute("checked");
+    landingPremiumPackageRadio.removeAttribute("checked");
+    landingBasicPackageRadio.setAttribute("disabled", "true");
+    landingPremiumPackageRadio.setAttribute("disabled", "true");
+
+    landingStandardPackageRadio.removeAttribute("disabled");
+    landingStandardPackageRadio.setAttribute("checked", "true");
+  } else if (choosenPackage.textContent === "Premium Package") {
+    landingBasicPackageRadio.removeAttribute("checked");
+    landingStandardPackageRadio.removeAttribute("checked");
+    landingBasicPackageRadio.setAttribute("disabled", "true");
+    landingStandardPackageRadio.setAttribute("disabled", "true");
+
+    landingPremiumPackageRadio.removeAttribute("disabled");
+    landingPremiumPackageRadio.setAttribute("checked", "true");
+  }
+};
+
+// const moreText = document.getElementById("more");
+// const moreBtn = document.getElementById("more-button");
+
+// moreBtn.addEventListener("click", () => {
+//   moreText.style.display = "block";
+// });
+
+// Function to toggle visibility of description
+function toggleDescription(classname) {
+  const descriptionElements = document.querySelectorAll("." + classname);
+
+  for (const element of descriptionElements) {
+    const button = element.nextElementSibling; // Assumes button is the next sibling
+    button.addEventListener("click", function () {
+      if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block";
+        button.textContent = "Less";
+      } else {
+        element.style.display = "none";
+        button.textContent = "More";
+      }
+    });
+  }
+}
+
+// Call the function for each class
+toggleDescription("premium-description");
+toggleDescription("other-description");
